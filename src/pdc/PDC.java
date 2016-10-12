c3307b344a1abcbc14c168ecddaa246ce3ea5e0f
gepr <gepr@tempusdictum.com>
2016-10-12 16:42:23 -0700

    • Run diffs on the .js files in the directories as well. • Refrain from any output if the files match. • Reformat .properties preamble output to look like that of diffutils

--- src/pdc/PDC.java
+++ src/pdc/PDC.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 - Regents of the University of California, San
+ * Copyright 2015-2016 - Regents of the University of California, San
  * Francisco.
  *
  * This program is distributed in the hope that it will be useful, but
@@ -50,22 +50,27 @@ public class PDC {
     dn[0] = cla.getOptionValue("dir0");
     dn[1] = cla.getOptionValue("dir1");
     
-    parseProperties(dn);
-    
+    ArrayList<ArrayList> files = buildFileList(dn, ".properties");
+    ArrayList<ArrayList> jsFiles = buildFileList(dn, ".js");
+    files.get(0).addAll(jsFiles.get(0));
+    files.get(1).addAll(jsFiles.get(1));
+    parseProperties(dn, files);
   }
 
-  public static void parseProperties(String[] dn) {
+  private static ArrayList<ArrayList> buildFileList(String[] dn, String suffix) {
     File[] dirs = new File[dn.length]; // get dirs from names
-    
     // get all the .properties files in each dir
     ArrayList<ArrayList> files = new ArrayList<>(2);
     for (int dNdx=0 ; dNdx<dn.length ; dNdx++) {
       dirs[dNdx] = new File(dn[dNdx]);
-      File[] file_array = dirs[dNdx].listFiles((File dir, String name) -> (name.endsWith(".properties") ));
+      File[] file_array = dirs[dNdx].listFiles((File dir, String name) -> (name.endsWith(suffix) ));
       ArrayList<File> file_list = new ArrayList(Arrays.asList(file_array));
       files.add(file_list);
     }
-    
+    return files;
+  }
+  
+  public static void parseProperties(String[] dn, ArrayList<ArrayList> files) {
     // for each file in each dir, check for that file in the other dir
     for (int lNdx=0 ; lNdx< 2 ; lNdx++) {
       ArrayList<File> this_list = files.get(lNdx);
@@ -76,10 +81,10 @@ public class PDC {
           System.out.println(f+" only in "+dn[lNdx]);
         else if (lNdx==0) {
           File[] fn = {f, that_file};
-          if (!f.getName().contains("hepstructspec")) {
-            diffPropertiesFiles(fn);
-          } else {
+          if (f.getName().contains("hepstructspec") || f.getName().contains(".js")) {
             diffTextFiles(fn);
+          } else {
+            diffPropertiesFiles(fn);
           }
         }
       }
@@ -114,7 +119,8 @@ public class PDC {
   }
   
   public static void diffPropertiesFiles(File[] fn) {
-    System.out.println("Parsing "+fn[0]+" and "+fn[1]);
+    boolean diffsFound = false;
+    StringBuffer output = new StringBuffer("--- "+fn[0]+"\n+++ "+fn[1]);
     ParameterDatabase[] pd = new ParameterDatabase[2];
     try {
       for (int ndx=0 ; ndx<2 ; ndx++)
@@ -128,13 +134,19 @@ public class PDC {
         String thisKey = (String)me.getKey();
         Object thisVal = me.getValue();
         ParameterDatabase that = pd[(ndx+1)%2];
-        if (!that.exists(new ec.util.Parameter((String)me.getKey()), null)) System.out.println(me.getKey().toString() + ":  only in file "+ndx);
-        else if (ndx==0) {
+        if (!that.exists(new ec.util.Parameter((String)me.getKey()), null)) {
+          output.append(me.getKey().toString() + ":  only in file "+ndx+"\n");
+          diffsFound = true;
+        } else if (ndx==0) {
           Object thatVal = that.getProperty(thisKey);
-          if (!thisVal.equals(thatVal)) System.out.print(thisKey+":\n - "+thisVal+"\n + "+thatVal+"\n");
+          if (!thisVal.equals(thatVal)) {
+            output.append(thisKey+":\n- "+thisVal+"\n+ "+thatVal+"\n");
+            diffsFound = true;
+          }
         }
       }
     }
+    if (diffsFound) System.out.println(output.toString());
   }
   
   static void printUsage() {
